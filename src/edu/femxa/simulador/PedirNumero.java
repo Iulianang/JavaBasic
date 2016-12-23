ecdbc2d0bc23a1f49ab53c6300cd22a61a790602
NFSICA <NFSICA@192.168.1.132>
2016-12-23 11:47:19 +0100

    Actualizado PedirNumero

--- src/edu/femxa/simulador/PedirNumero.java
+++ src/edu/femxa/simulador/PedirNumero.java
@@ -3,44 +3,54 @@ package edu.femxa.simulador;
 import java.util.Scanner;
 
 public class PedirNumero {
-	//TODO
+	
 	
 	public static int pedirNumero ()
 	{
 		int pedirNumero = 0;
 		Scanner scanner = null;
+		scanner = new Scanner(System.in); 
+		pedirNumero = scanner.nextInt();
 		
-			System.out.println("Introduce el numero: ");
-			scanner = new Scanner(System.in); 
-			pedirNumero = scanner.nextInt();
-			int x = (int) (100*Math.random()+1);
-			int contador = 0;	
 			
-			while (contador<5)
-			{
-				if (pedirNumero<x)
-				{
-					System.out.println("El numero indicado es menor, puebe de nuevo:");
-				}
-					else if (pedirNumero>x)
-						{
-							System.out.println("El numero indicado es mayor, pruebe de nuevo:");
-						}
-						else if (pedirNumero == x)
-							{
-							System.out.println("Ha acertado el numero, felicidades");
-							}
-							else 
-							{
-								System.out.println("Ha agotado sus intentos, manco");
-							}
-			}
 		return pedirNumero;
 	}
 	
 	public static void main(String[] args) {
 		
+		int numP = 0;
+		System.out.println("Introduce el numero: ");
+		numP = pedirNumero ();
+		int x = (int) (100*Math.random()+1);
+		int contador = 0;	
+		boolean correcto = false;
+		
+		do 
+		{
+			if (numP<x)
+			{
+				System.out.println("El numero indicado es menor, pruebe de nuevo:");
+				numP = pedirNumero();
+			}
+				else if (numP>x)
+					{
+						System.out.println("El numero indicado es mayor, pruebe de nuevo:");
+						numP = pedirNumero();
+					}
+					else if (numP == x)
+						{
+							correcto = true;
+							System.out.println("Ha acertado el numero, felicidades");
+						}
+							
 			
+			
+			contador++;
+			}while (contador<5 && !correcto); 
+			System.out.println(x);
+				
+		}
+		
 		
 	}
 	
@@ -50,4 +60,4 @@ public class PedirNumero {
 		
 	
 
-}
+
