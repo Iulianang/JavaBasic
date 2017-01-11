1a2a893ffeac40a2654c8fd232f770b6cf47a3a6
NFSICA <NFSICA@192.168.1.132>
2017-01-11 13:02:10 +0100

    Deberes

--- src/edu/femxa/sinespacios/Main.java
+++ src/edu/femxa/sinespacios/Main.java
@@ -1,5 +1,39 @@
 package edu.femxa.sinespacios;
 
+import java.util.Scanner;
+
 public class Main {
 
+	public Main() {
+		String frase = null;
+		String sFrase = null;
+		Scanner scan = null;
+			scan = new Scanner(System.in);
+			frase = scan.next();
+			
+		sFrase = sinEspacio(frase);
+		
+		System.out.println("Frase originar: " + frase);
+		System.out.println("Frase sin espacios: "+ sFrase);
+	}
+	
+	
+	public String sinEspacio (String normal)
+	{
+		String sEspacio = null;
+		char aux = 0;
+			for (int i = 0; i < normal.length(); i++) 
+			{
+				aux = normal.charAt(i);
+				
+				if(aux != ' ')
+					
+					sEspacio= sEspacio + aux;
+					
+			}
+		
+		return sEspacio;
+	}
+
+	
 }
