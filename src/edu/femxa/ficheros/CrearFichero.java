9122b31f34d8eb72ee71b49973596ecdbb0ebd2f
Iulianang <iulian.anghel19@gmail.com>
2017-01-13 16:20:30 +0100

    Terminado creado/ borrado de ficheros

--- src/edu/femxa/ficheros/CrearFichero.java
+++ src/edu/femxa/ficheros/CrearFichero.java
@@ -1,39 +1,55 @@
 package edu.femxa.ficheros;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
-
 public class CrearFichero {
 	
 	public static void main(String[] args) {
 		
 		Scanner scan = null;
 		scan = new Scanner(System.in);
-		System.out.println("Introduzca el numero del fichero: ");
+		System.out.println("Introduzca el numero del fichero a crear: ");
 			String nombre = scan.nextLine();
 		
 		crear(nombre);
+		System.out.println("Introduzca el nombre del fichero a borrar: ");
+		String nombre2 = scan.nextLine();
+		borrar(nombre2);
 		
 	}
 
-	public static boolean crear (String cad)
+	public static void crear (String cad)
 	{
-		boolean creado = false;
 		File fichero = null;
-		fichero = new File("fichero");
+		fichero = new File(cad);
 		
-		if(!creado)
-			{
-			//TODO
-				boolean crearArchivo = fichero.createNewFile();
-				System.out.println("El fichero se ha creado.");
-				
-			}else
-			{
-				System.out.println("El fichero no se ha creado.");
-			}
+		try {
+			if(fichero.createNewFile())
+				{
+					System.out.println("El fichero se ha creado.");
+					
+				}else
+				{
+					System.out.println("El fichero no se ha creado.");
+				}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		return creado;
+		
+	}
+	public static void borrar (String cad)
+	{
+		File fichero = null;
+		fichero = new File(cad);
+		if (fichero.isFile()){
+			
+			fichero.delete();
+			System.out.println("El fichero se ha borrado.");
+		}else{
+			System.out.println("El fichero indicado no se puede borrar");
+		}
 		
 	}
 }
